19563878abdf100f4e2bfe867844839ece45b9db
Gavin Gee <galvindgee@gmail.com>
2021-05-24 16:20:39 -0700

    Title Screen Fade

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fader : MonoBehaviour
+{
+    public Image img;
+
+    private void Start()
+    {
+        //img = GameObject.Find("Black Screen").GetComponent<Image>();
+        img.enabled = true;
+        if (img)
+        {
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        Debug.Log("Fading ...");
+        img.CrossFadeAlpha(0, .75f, false);
+        yield return new WaitForSeconds(.75f);
+    }
+
+    public void FadeSceneChange()
+    {
+        StartCoroutine(SceneChangeHelper());
+    }
+
+    private IEnumerator SceneChangeHelper()
+    {
+        Debug.Log(img.color.a);
+        img.CrossFadeAlpha(1, .75f, false);
+        yield return new WaitForSeconds(.75f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
